9b0f41053d62fb25b770afbf3399d5e59c963177
toriqultonu <toriqultonu22@gmail.com>
2025-07-16 11:17:37 +0600

    Add GraphTraverser and TestGraph classes with depth-first traversal implementation

--- src/Vertex.java
+++ src/Vertex.java
@@ -29,7 +29,7 @@ public class Vertex {
     public void print(boolean showWeight){
         String message = "";
 
-        if(this.edges.size() == 0){
+        if(this.edges.isEmpty()){
             System.out.println(this.data + "-->");
             return;
         }
